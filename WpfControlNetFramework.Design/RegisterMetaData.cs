ca202ec6c6f8eba1a74548a5fddcecf4d53ba095
Chris Hamilton <chris@tracktownsoftware.com>
2020-01-16 12:41:18 -0800

    Added design-time contextmenu for WpfControlNetFramework.ButtonWithDesignTime

---
+++ WpfControlNetFramework.Design/RegisterMetaData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Windows.Design;
+using Microsoft.Windows.Design.Features;
+using Microsoft.Windows.Design.Metadata;
+
+// The ProvideMetadata assembly-level attribute indicates to designers
+// that this assembly contains a class that provides an attribute table. 
+[assembly: ProvideMetadata(typeof(WpfControlNetFramework.Design.RegisterMetaData))]
+
+namespace WpfControlNetFramework.Design
+{
+    public class RegisterMetaData : IProvideAttributeTable
+    {
+        public AttributeTable AttributeTable
+        {
+            get
+            {
+                AttributeTableBuilder builder = new AttributeTableBuilder();
+
+                // Add the menu provider to the design-time metadata.
+                builder.AddCustomAttributes(
+                    typeof(WpfControlNetFramework.ButtonWithDesignTime),
+                    new FeatureAttribute(typeof(CustomContextMenuProvider)));
+
+                return builder.CreateTable();
+            }
+        }
+    }
+}
